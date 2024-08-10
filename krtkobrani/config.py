e7590ce689d924c892de99e7e4a94e554fcf19a4
filakrad <filakrad@gmail.com>
2024-08-10 22:51:56 +0200

    config relative path

--- krtkobrani/config.py
+++ krtkobrani/config.py
@@ -1,9 +1,11 @@
 import json
-
+from pathlib import Path
+THIS_FOLDER = Path(__file__).parent.parent.resolve()
+my_file = THIS_FOLDER / "config.json"
 
 def read_json(file_path):
     with open(file_path, "r") as f:
         return json.load(f)
 
 
-config = read_json("config.json")
+config = read_json(my_file)
