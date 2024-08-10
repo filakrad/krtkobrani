bdca23a97ad6965d0c3b2cb969d51a902c98b868
filakrad <filakrad@gmail.com>
2024-08-10 22:14:38 +0200

    runserver fix

--- run_server.py
+++ run_server.py
@@ -1,5 +1,5 @@
-from krtkobrani import main
+from krtkobrani.main import app
 
 if __name__ == '__main__':
     # Run the app server with default Flask port
-    main.app.run(port=4444, host='0.0.0.0')
+    app.run(port=4444, host='0.0.0.0')
