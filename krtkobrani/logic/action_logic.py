4f0dc65a681800bfdc2f4d7eeef5cec29d32649f
Stepan Filakovsky <stipi007seznam.cz>
2024-10-11 22:52:59 +0200

    zpet

--- krtkobrani/logic/action_logic.py
+++ krtkobrani/logic/action_logic.py
@@ -21,7 +21,7 @@ def sanitize_string(the_string):
 
 
 def start_game():
-    all_teams = db.session.query(Team).all()
+    all_teams = db.session.query(Team).filter_by(is_admin= 0).all()
     first_site = db.session.query(Site).filter_by(site_number=1).first()
     start_time = datetime.utcnow()
     for team in all_teams:
